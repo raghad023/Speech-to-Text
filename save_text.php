16bdd88cb4133eec94ae4407d0b565369e89c2dc
raghad023 <rghad023@gmail.com>
2024-08-03 10:40:59 +0300

    Add files via upload

---
+++ save_text.php
@@ -0,0 +1,32 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "speech_to_text";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $text = $_POST['text'];
+
+    $stmt = $conn->prepare("INSERT INTO recordings (text) VALUES (?)");
+    if ($stmt) {
+        $stmt->bind_param("s", $text);
+        if ($stmt->execute()) {
+            echo "New record created successfully";
+        } else {
+            echo "Execute error: " . $stmt->error;
+        }
+        $stmt->close();
+    } else {
+        echo "Prepare error: " . $conn->error;
+    }
+}
+
+$conn->close();
+?>
